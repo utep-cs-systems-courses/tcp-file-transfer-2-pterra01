09869cd59268a8fa17c9a146305fe660246a2f94
Paola Terrazas <pterrazas3@miners.utep.edu>
2020-10-18 15:44:02 -0700

    Copied files for threading

---
+++ file-transfer-lab/framedClient.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python3
+
+# Echo client program
+import socket, sys, re
+
+sys.path.append("../lib")       # for params
+import params
+
+from encapFramedSock import EncapFramedSock
+
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+
+progname = "framedClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+if usage:
+    params.usage()
+
+
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+addrFamily = socket.AF_INET
+socktype = socket.SOCK_STREAM
+addrPort = (serverHost, serverPort)
+
+sock = socket.socket(addrFamily, socktype)
+
+if sock is None:
+    print('could not open socket')
+    sys.exit(1)
+
+sock.connect(addrPort)
+
+fsock = EncapFramedSock((sock, addrPort))
+
+for i in range(2):
+    print("sending hello world")
+    fsock.send( b"hello world", debug)
+    print("received:", fsock.receive(debug))
---
+++ file-transfer-lab/framedThreadServer.py
@@ -0,0 +1,51 @@
+#! /usr/bin/env python3
+
+import sys
+sys.path.append("../lib")       # for params
+import re, socket, params, os
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+    params.usage()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+bindAddr = ("127.0.0.1", listenPort)
+lsock.bind(bindAddr)
+lsock.listen(5)
+print("listening on:", bindAddr)
+
+from threading import Thread;
+from encapFramedSock import EncapFramedSock
+
+class Server(Thread):
+    def __init__(self, sockAddr):
+        Thread.__init__(self)
+        self.sock, self.addr = sockAddr
+        self.fsock = EncapFramedSock(sockAddr)
+    def run(self):
+        print("new thread handling connection from", self.addr)
+        while True:
+            payload = self.fsock.receive(debug)
+            if debug: print("rec'd: ", payload)
+            if not payload:     # done
+                if debug: print(f"thread connected to {addr} done")
+                self.fsock.close()
+                return          # exit
+            payload += b"!"             # make emphatic!
+            self.fsock.send(payload, debug)
+        
+
+while True:
+    sockAddr = lsock.accept()
+    server = Server(sockAddr)
+    server.start()
